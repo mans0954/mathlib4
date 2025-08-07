89802955cf1717829a9c4cd79b24ee470f52f29e
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-08-07 16:51:27 +0100

    Initial WiP

--- Mathlib/Analysis/LocallyConvex/StrongTopology.lean
+++ Mathlib/Analysis/LocallyConvex/StrongTopology.lean
@@ -63,6 +63,10 @@ variable [Semiring R] [PartialOrder R]
 variable [NormedField 𝕜₁] [NormedField 𝕜₂] [Module 𝕜₁ E] [Module 𝕜₂ F] {σ : 𝕜₁ →+* 𝕜₂}
 variable [Module R F] [ContinuousConstSMul R F] [LocallyConvexSpace R F] [SMulCommClass 𝕜₂ R F]
 
+#check ContinuousLinearMap.topologicalSpace
+
+instance : TopologicalSpace (E →SL[σ] F) := by exact topologicalSpace
+
 instance instLocallyConvexSpace : LocallyConvexSpace R (E →SL[σ] F) :=
   UniformConvergenceCLM.locallyConvexSpace R _ ⟨∅, Bornology.isVonNBounded_empty 𝕜₁ E⟩
     (directedOn_of_sup_mem fun _ _ => Bornology.IsVonNBounded.union)
@@ -70,3 +74,166 @@ instance instLocallyConvexSpace : LocallyConvexSpace R (E →SL[σ] F) :=
 end ContinuousLinearMap
 
 end BoundedSets
+
+
+
+-- Bilinear
+
+namespace ContinuousLinearMap
+
+section prod
+
+variable {G : Type*} [TopologicalSpace G] [AddCommGroup G] [IsTopologicalAddGroup G]
+
+variable [Semiring R] [PartialOrder R]
+variable {𝕜₁ 𝕜₂ 𝕜₃ : Type*} [NormedField 𝕜₁] [NormedField 𝕜₂] [NormedField 𝕜₃]
+variable [Module 𝕜₁ E] [Module 𝕜₂ F] [Module 𝕜₃ G]
+
+variable [Module R E] [ContinuousConstSMul R E] [LocallyConvexSpace R E] [SMulCommClass 𝕜₁ R E]
+variable [Module R F] [ContinuousConstSMul R F] [LocallyConvexSpace R F] [SMulCommClass 𝕜₂ R F]
+variable [Module R G] [ContinuousConstSMul R G] [LocallyConvexSpace R G] [SMulCommClass 𝕜₃ R G]
+
+variable [ContinuousConstSMul 𝕜₃ G]
+
+--variable (B : E × F →L[𝕜] G)
+
+#check RingHom.prod
+
+#check MonoidHom.prod
+
+#check LinearMap.prod
+
+#check LinearMap.llcomp
+
+variable {σ₁₂ : 𝕜₁ →+* 𝕜₂} {σ₂₃ : 𝕜₂ →+* 𝕜₃} {σ₁₃ : 𝕜₁ →+* 𝕜₃} -- [RingHomCompTriple σ₁₂ σ₂₃ σ₁₃]
+
+--variable (f : E →SL[σ₁₃] F →SL[σ₂₃] G)
+
+variable (y : F)
+
+instance : TopologicalSpace (F →SL[σ₂₃] G) := by exact instTopologicalSpace
+
+variable (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) (x : E) -- (h : Continuous (B x))
+
+#check B x
+
+#check ContinuousLinearMap.mk
+
+#check ContinuousLinearMap.mk (B x) h
+
+structure sep_con (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) : Prop where
+  left : ∀ (x : E), Continuous (B x)
+  right : ∀ (y : F), Continuous (B.flip y)
+
+lemma sepcon1 (B : E →SL[σ₁₃] F →SL[σ₂₃] G) : sep_con B.toLinearMap₁₂ where
+  left x := (B x).2
+  right := by
+    intro y
+    
+
+
+
+def mkstp1 (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) (h : sep_con B) : E →ₛₗ[σ₁₃] F →SL[σ₂₃] G where
+  toFun x := ⟨B x, h.left x⟩
+  map_add' _ _:= by
+    simp only [map_add]
+    rfl
+  map_smul' _ _ := by
+    simp only [LinearMap.map_smulₛₗ]
+    rfl
+
+omit [IsTopologicalAddGroup F] in
+lemma mkstp_apply (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) (h : sep_con B) (x : E) : mkstp1 B h x = B x := rfl
+
+def mkContinuousOfSepCon (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G)
+    (h : sep_con B) : E →SL[σ₁₃] F →SL[σ₂₃] G := ⟨mkstp1 B h, by
+    simp only [AddHom.toFun_eq_coe, LinearMap.coe_toAddHom]
+    refine continuous_iff_continuousAt.mpr ?_
+    intro x
+
+    --simp_rw [mkstp_apply]
+    norm_cast
+    simp [mkstp1, mkstp_apply]
+
+    convert h.right _
+    sorry⟩
+
+#check sep_con
+
+
+
+--def mk₂ (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) (h : ∀ (y : F), Continuous (B.flip y)) :
+
+
+
+lemma test1 (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G) : sep_con B ↔ ∀ (y : F), Continuous (B.flip y) ∧ Continuous B := sorry
+
+
+
+def l1 (f : E →SL[σ₁₃] F →SL[σ₂₃] G) : (E × F) → G := fun (x, y) => f x y
+
+-- Bourbaki TVS III.28-30 sufficient and necessary conditions for separate continuity
+
+
+
+
+lemma c1 : Continuous (l1 f) where
+  isOpen_preimage := by
+    intro U hU
+
+
+
+#check (f : E × F → G)
+
+variable (X : E × F) (K : 𝕜₁ × 𝕜₂)
+
+#check K • X
+
+
+--lemma p1 (B : E × F →L[𝕜] G) :
+
+end prod
+
+
+
+
+
+variable [Semiring R] [PartialOrder R]
+variable {𝕜₃ : Type*} [NormedField 𝕜₁] [NormedField 𝕜₂] [NormedField 𝕜₃]
+  {σ₁₂ : 𝕜₁ →+* 𝕜₂} {σ₂₃ : 𝕜₂ →+* 𝕜₃} {σ₁₃ : 𝕜₁ →+* 𝕜₃}
+
+   {G : Type*} [TopologicalSpace G] [AddCommGroup G]
+  --{M₄ : Type*} [TopologicalSpace M₄] [AddCommMonoid M₄]
+variable [Module 𝕜₁ E] --[Module 𝕜₁ M'₁]
+  [Module 𝕜₂ F] [Module 𝕜₃ G]
+variable [Module R E] [ContinuousConstSMul R E] [LocallyConvexSpace R E] [SMulCommClass 𝕜₁ R E]
+variable [Module R F] [ContinuousConstSMul R F] [LocallyConvexSpace R F] [SMulCommClass 𝕜₂ R F]
+variable [Module R G] [ContinuousConstSMul R G] [LocallyConvexSpace R G] [SMulCommClass 𝕜₃ R G]
+variable [IsTopologicalAddGroup G] [ContinuousConstSMul 𝕜₃ G]
+
+variable [RingHomIsometric σ₂₃] [RingHomIsometric σ₁₃] [RingHomCompTriple σ₁₂ σ₂₃ σ₁₃]
+
+variable (B : E →ₛₗ[σ₁₃] F →ₛₗ[σ₂₃] G)
+
+#check  Continuous B
+
+
+variable (L : E →SL[σ₁₃] F →SL[σ₂₃] G)
+
+#check (L : E × F → G)
+
+lemma contprod : Continuous L := sorry
+
+
+/-- Flip the order of arguments of a continuous bilinear map.
+For a version bundled as `LinearIsometryEquiv`, see
+`ContinuousLinearMap.flipL`. -/
+def flip (f : E →SL[σ₁₃] F →SL[σ₂₃] G) : F →SL[σ₂₃] E →SL[σ₁₃] G :=
+  LinearMap.mkContinuous₂
+    (LinearMap.mk₂'ₛₗ σ₂₃ σ₁₃ (fun y x => f x y) (fun x y z => (f z).map_add x y)
+      (fun c y x => (f x).map_smulₛₗ c y) (fun z x y => by simp only [f.map_add, add_apply])
+        (fun c y x => by simp only [f.map_smulₛₗ, smul_apply]))
+    ‖f‖ fun y x => (f.le_opNorm₂ x y).trans_eq <| by simp only [mul_right_comm]
+
+
+end ContinuousLinearMap
